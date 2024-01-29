9920a083b1aef4276ad34b849535845792ba68f7
rashaaz <rashaabdelaziz111@gmail.com>
2024-01-29 02:23:32 -0800

    modfy

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Calculate the height of a binary tree
  * @tree: Pointer to the root node of the tree
  *
- * Return: Height of the tree
+ * Return: Height of the tree.
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
