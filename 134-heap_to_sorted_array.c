a70f57c22ee45d7466215557b29a973a7f57b68c
randaaz <randaaziz972@gmail.com>
2024-01-29 20:36:02 -0800

    try to fix the error

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,6 +1,14 @@
 #include "binary_trees.h"
 
 /**
+ * heap_to_sorted_array - Converts a binary heap to a sorted array
+ * @heap: Pointer to the root of the heap
+ * @size: Pointer to the size of the resulting sorted array
  *
- *
+ * Return: Pointer to the sorted array, or NULL on failure
  */
+
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (NULL);
+}
