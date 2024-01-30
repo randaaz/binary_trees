a4cef97b368a0337d88f1ff4be6aa29e24c233c3
rashaaz <rashaabdelaziz111@gmail.com>
2024-01-30 11:41:34 -0800

    modfy

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,48 +1,52 @@
 #include "binary_trees.h"
 
 /**
- * b_t_h_b - function to compute the height of a binary tree
+ * binary_tree_height_me - function to compute the height of a binary tree
  * @tree: pointer to the root node of the tree to compute the height
  *
  * Return: height of the binary tree
  */
 
-size_t b_t_h_b(const binary_tree_t *tree)
+size_t binary_tree_height_me(const binary_tree_t *tree)
 {
-	size_t s = 0;
-	size_t m = 0;
+	size_t l = 0;
+	size_t r = 0;
+
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	l = binary_tree_height_me(tree->left);
+	r = binary_tree_height_me(tree->right);
+	if (l > r)
+	{
+		return (l + 1);
+	}
 	else
 	{
-		if (tree)
-		{
-			s = tree->left ? 1 + b_t_h_b(tree->left) : 1;
-			m = tree->right ? 1 + b_t_h_b(tree->right) : 1;
-		}
-		return ((s > m) ? s : m);
+		return (r + 1);
 	}
 }
-
 /**
  * binary_tree_balance - function to compute the balanc
  * @tree: pointer to the root node of the tree
  *
  * Return: balance factor of the binary tree
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int r = 0, l = 0, t = 0;
+	int lft = 0;
+	int rigt = 0;
 
 	if (tree)
 	{
-		l = ((int)b_t_h_b(tree->left));
-		r = ((int)b_t_h_b(tree->right));
-		t = l - r;
+		lft = (int)binary_tree_height_me(tree->left);
+		rigt = (int)binary_tree_height_me(tree->right);
 	}
-	return (t);
+	return (lft - rigt);
 }
