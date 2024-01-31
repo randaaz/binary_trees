73f87dd30d660d4a2da48b52da2986eefeb502b0
rashaaz <rashaabdelaziz111@gmail.com>
2024-01-30 21:39:59 -0800

    modfy tske 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,36 +7,14 @@
  * Return: Pointer to the sibling node,
  * or NULL if no sibling or if node is NULL.
  */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *p;
-
 	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-	if (node->parent->left == NULL || node->parent->right == NULL)
-	{
-		return (NULL);
-	}
-	if (node->parent->right != node)
-	{
-		if (node->parent->left == NULL || node->parent->right != NULL)
-		{
-			p = node->parent->right;
-			return (p);
-
-		}
-	}
-	if (node->parent->left != node)
-	{
-		if (node->parent->left != NULL || node->parent->right == NULL)
-		{
-			p = node->parent->left;
-			return (p);
-
-		}
-	}
-	binary_tree_sibling(node->parent->left);
-	binary_tree_sibling(node->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
